17e4856b46ce45b16669564f4e3795207929c451
Sean McKinley <sean.mckinley@outlook.com>
2018-07-20 20:57:05 -0600

    This commit adds the SNMP class, with full functionality. Turns out adding classes is easy once you've got it all abstracted out into shared methods, who knew?

---
+++ lib/purest/snmp.rb
@@ -0,0 +1,23 @@
+module Purest
+  class SNMP < Purest::APIMethods
+    @access_methods = %i[get create update delete]
+
+    GET_PARAMS = [:engine_id]
+
+    def get(options = nil)
+      super(options, 'snmp', GET_PARAMS)
+    end
+
+    def create(options = nil)
+      super(options, 'snmp')
+    end
+
+    def update(options = nil)
+      super(options, 'snmp')
+    end
+
+    def delete(options = nil)
+      super(options, 'snmp')
+    end
+  end
+end
