85a5e531e06a1cfe69f154a7f8f3acded1e7c811
Sean McKinley <sean.mckinley@outlook.com>
2018-07-23 08:01:53 -0600

    Adding a few unit tests for the Network class

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -41,7 +41,7 @@ describe Purest::Network do
   describe '#post' do
     context 'when creating a vlan interface' do
       it 'posts to the correct url' do
-        stub_request(:get, "#{Purest.configuration.url}/api/1.11/network/vif/vlan1").
+        stub_request(:post, "#{Purest.configuration.url}/api/1.11/network/vif/vlan1").
           with(
             body: "{\"name\":\"vlan1\",\"subnet\":\"subnet10\"}",
             headers: {
@@ -52,6 +52,39 @@ describe Purest::Network do
             to_return(status: 200, body: JSON.generate([]), headers: {})
 
         vlan = Purest::Network.create(:name => 'vlan1', :subnet => 'subnet10')
+      end
+    end
+  end
+  describe '#put' do
+    context 'when updating attributes' do
+      it 'posts to the correct url with the correct HTTP body' do
+        stub_request(:put, "#{Purest.configuration.url}/api/1.11/network/alpha.eth0").
+          with(
+            body: "{\"name\":\"alpha.eth0\",\"mtu\":2000}",
+            headers: {
+       	    'Accept'=>'*/*',
+       	    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	    'User-Agent'=>'Faraday v0.15.2'
+            }).
+            to_return(status: 200, body: JSON.generate([]), headers: {})
+
+        interface = Purest::Network.update(:name => 'alpha.eth0', :mtu => 2000)
+      end
+    end
+  end
+  describe '#delete' do
+    context 'when updating attributes' do
+      it 'posts to the correct url with the correct HTTP body' do
+        stub_request(:delete, "#{Purest.configuration.url}/api/1.11/network/alpha.eth0").
+          with(
+            headers: {
+       	    'Accept'=>'*/*',
+       	    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	    'User-Agent'=>'Faraday v0.15.2'
+            }).
+            to_return(status: 200, body: JSON.generate([]), headers: {})
+
+        interface = Purest::Network.delete(:name => 'alpha.eth0')
       end
     end
   end
